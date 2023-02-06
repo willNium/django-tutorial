7d4f25d492e96250fad4668bf81363ba0dbf495f
Billy Johnson <wjohnson1000@gmail.com>
2023-02-06 11:41:43 -0500

    ignores __pycache__ doesnt publish questions with no choices allows admins to see questions with no choices

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.test import TestCase
 from django.utils import timezone
 
-from .models import Question
+from .models import Question, Choice
 
 def create_question(question_text, days):
     """
@@ -13,6 +13,14 @@ def create_question(question_text, days):
     """
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+def create_choice(choice_text, question_id):
+    """
+    Create a choice for a question
+    """
+
+    return Choice.objects.create(choice_text=choice_text, question_id=question_id)
 
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
@@ -49,8 +57,25 @@ class QuestionIndexViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No polls are available")
 
+    def test_question_no_choice(self):
+        question = create_question("how old is this question?", -30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available")
+
+    def test_question_no_choice_is_admin(self):
+        question = create_question("how old is this question?", -30)
+        self.client.cookies.load({ 'sessionid': '123' })
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [question]
+        )
+
     def test_past_question(self):
         question = create_question("how old is this question?", -30)
+        create_choice("its so new", question.id)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
             response.context['latest_question_list'],
@@ -65,6 +90,7 @@ class QuestionIndexViewTests(TestCase):
 
     def test_future_and_past_questions(self):
         old_question = create_question("is it old?", -30)
+        create_choice("its so new", old_question.id)
         create_question("is it new?", 30)
 
         response = self.client.get(reverse('polls:index'))
@@ -75,9 +101,11 @@ class QuestionIndexViewTests(TestCase):
 
     def test_multiple_future_and_past_questions(self):
         old_question = create_question("is it old?", -30)
+        create_choice("its so new", old_question.id)
         old_question_2 = create_question("is it old?", -32)
-        new_question = create_question("is it new?", 30)
-        new_question_2 = create_question("is it new?", 31)
+        create_choice("its so new", old_question_2.id)
+        create_question("is it new?", 30)
+        create_question("is it new?", 31)
 
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
@@ -95,8 +123,49 @@ class QuestionDetailViewTests(TestCase):
 
     def test_past_question(self):
         past_question = create_question("can see it", -30)
+        create_choice("yerp", past_question.id)
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, past_question.question_text)
+
+    def test_past_question_no_choice(self):
+        past_question = create_question("can see it", -30)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question_no_choice_is_admin(self):
+        past_question = create_question("can see it", -30)
+        url = reverse('polls:detail', args=(past_question.id,))
+        self.client.cookies.load({ 'sessionid': '123' })
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, past_question.question_text)
+
+class QuestionResultsViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question("cant see it", 30)
+        create_choice("dont look", future_question.id)
+        url = reverse('polls:results', args=(future_question.id,))
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question("can see it", -30)
+        create_choice("see look", past_question.id)
+        url = reverse('polls:results', args=(past_question.id,))
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, past_question.question_text)
+
+    def test_past_question_no_choice(self):
+        past_question = create_question("can see it", -30)
+        url = reverse('polls:results', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.db.models import Count
 
 from .models import Choice, Question
 
@@ -10,24 +11,80 @@ from .models import Choice, Question
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
+    is_admin = False
+
+    def dispatch(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        cookies = request.headers['Cookie']
+        if 'sessionid' in cookies:
+            self.is_admin = True
+
+        return super().dispatch(request, *args, **kwargs)
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        if self.is_admin == True:
+            return (Question.objects
+                    .filter(pub_date__lte=timezone.now())
+                    .order_by('-pub_date')[:5]
+                    )
+        else:
+            """Return the last five published questions."""
+            return (Question.objects
+                .filter(pub_date__lte=timezone.now())
+                .annotate(num_choices=Count('choice'))
+                .filter(num_choices__gt=0)
+                .order_by('-pub_date')[:5]
+            )
+
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    is_admin = False
+
+    def dispatch(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        if 'sessionid' in request.COOKIES:
+            self.is_admin = True
+
+        return super().dispatch(request, *args, **kwargs)
 
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
         """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+
+        if self.is_admin == True:
+            return Question.objects.filter(pub_date__lte=timezone.now())
+        else:
+            return (Question.objects
+                .filter(pub_date__lte=timezone.now())
+                .annotate(num_choices=Count('choice'))
+                .filter(num_choices__gt=0)
+            )
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+    is_admin = False
+
+    def dispatch(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        if 'sessionid' in request.COOKIES:
+            self.is_admin = True
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+
+        if self.is_admin == True:
+            return Question.objects.filter(pub_date__lte=timezone.now())
+        else:
+            return (Question.objects
+                    .filter(pub_date__lte=timezone.now())
+                    .annotate(num_choices=Count('choice'))
+                    .filter(num_choices__gt=0)
+                    )
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
